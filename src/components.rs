88700e20d182153e18751c7efc5e4baf183cd5e7
ColorfullMadness <podniebnyvader@gmail.com>
2023-05-24 01:18:53 +0200

    initial commit

---
+++ src/components.rs
@@ -0,0 +1,106 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::{prelude::*, utils::ldtk_pixel_coords_to_translation_pivoted, ldtk::all_some_iter::AllSomeIter};
+
+use std::collections::HashSet;
+
+use bevy_rapier2d::prelude::*;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Wall;
+
+#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
+pub struct WallBundle {
+    wall: Wall,
+}   
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Spawn;
+
+#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
+pub struct SpawnBundle {
+    spawn: Spawn,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Player;
+
+#[derive(Clone, Default, Bundle, LdtkEntity)]
+pub struct PlayerBundle {
+    #[sprite_bundle("player.png")]
+    #[bundle]
+    pub sprite_bundle: SpriteBundle,
+    #[from_entity_instance]
+    #[bundle]
+    pub collider_bundle: ColliderBundle,
+    pub player: Player,
+    #[worldly]
+    pub worldly: Worldly,
+
+    // Build Items Component manually by using `impl From<&EntityInstance>`
+    //#[from_entity_instance]
+    //items: Items,
+
+    // The whole EntityInstance can be stored directly as an EntityInstance component
+    #[from_entity_instance]
+    pub entity_instance: EntityInstance,
+}
+
+#[derive(Clone, Component, Debug, Eq, Default, PartialEq)]
+pub struct Health(i32);
+
+impl From<&EntityInstance> for Health {
+    fn from(entity_instance: &EntityInstance) -> Self {
+        Health(
+            entity_instance
+                .get_int_field("health")
+                .expect("health field should be correctly typed")
+                .to_owned()
+        )
+    }
+}
+
+#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
+pub struct ColliderBundle {
+    pub collider: Collider,
+    pub rigid_body: RigidBody,
+    pub velocity: Velocity,
+    pub rotation_constraints: LockedAxes,
+    pub gravity_scale: GravityScale,
+    pub friction: Friction,
+    pub density: ColliderMassProperties,
+}
+
+impl From<&EntityInstance> for ColliderBundle {
+    fn from(entity_instance: &EntityInstance) -> ColliderBundle {
+        let rotation_constraints = LockedAxes::ROTATION_LOCKED;
+
+        match entity_instance.identifier.as_ref() {
+            "Player" => ColliderBundle {
+                collider: Collider::cuboid(3., 6.),
+                rigid_body: RigidBody::Dynamic,
+                friction: Friction {
+                    coefficient: 0.0,
+                    combine_rule: CoefficientCombineRule::Min,
+                },
+                rotation_constraints,
+                ..Default::default()
+            },
+            "Mob" => ColliderBundle {
+                collider: Collider::cuboid(5., 5.),
+                rigid_body: RigidBody::KinematicVelocityBased,
+                rotation_constraints,
+                ..Default::default()
+            },
+            "Chest" => ColliderBundle {
+                collider: Collider::cuboid(8., 8.),
+                rigid_body: RigidBody::Dynamic,
+                rotation_constraints,
+                gravity_scale: GravityScale(1.0),
+                friction: Friction::new(0.5),
+                density: ColliderMassProperties::Density(15.0),
+                ..Default::default()
+            },
+            _ => ColliderBundle::default(),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+use bevy_rapier2d::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod components;
+mod systems;
+
+use systems::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugin(LdtkPlugin)
+        .add_plugin(WorldInspectorPlugin::default())
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        // Required to prevent race conditions between bevy_ecs_ldtk's and bevy_rapier's systems
+        .configure_set(LdtkSystemSet::ProcessApi.before(PhysicsSet::SyncBackend))
+        .insert_resource(RapierConfiguration {
+            gravity: Vec2::new(0.0, 0.0),
+            ..Default::default()
+        })
+        .insert_resource(LevelSelection::Uid(0))
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation { 
+                load_level_neighbors: false 
+            },
+            set_clear_color: SetClearColor::FromLevelBackground,
+            ..Default::default()
+        })
+        .add_startup_system(setup)
+        .add_system(spawn_player)
+        .add_system(spawn_wall_collision)
+        .add_system(movement)
+        .add_system(camera_fit_inside_current_level)
+        .register_ldtk_int_cell::<components::WallBundle>(1)
+        .register_ldtk_int_cell::<components::SpawnBundle>(2)
+        .register_ldtk_entity::<components::PlayerBundle>("Player")
+        .run();
+}
